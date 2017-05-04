3a182b6f55345b3a71acd44a584c379079887f5c
savodka <mmm@mAir.local>
2017-05-04 14:00:08 +0300

    send mail

--- js/my.js
+++ js/my.js
@@ -1,5 +1,19 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
 	//$("p").css("backgroundColor", "white");
 	});
 
 //.button
+*/
+$(document).ready(function(){
+    $("#form").submit(function() { //устанавливаем событие отправки для формы с id=form
+            var form_data = $(this).serialize(); //собераем все данные из формы
+            $.ajax({
+            type: "POST", //Метод отправки
+            url: "send.php", //путь до php фаила отправителя
+            data: form_data,
+            success: function() {
+                   //код в этом блоке выполняется при успешной отправке сообщения
+                   alert("Ваше сообщение отправлено!");
+            });
+    });
+}); 
