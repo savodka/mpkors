60767a95c9e52307c4e754ee6b67f76e0a24b328
savodka <savodka@bk.ru>
2017-05-04 17:09:53 +0300

    Add files via upload

--- js/my.js
+++ js/my.js
@@ -1,19 +1,11 @@
-/*$(document).ready(function(){
-	//$("p").css("backgroundColor", "white");
-	});
-
-//.button
-*/
-$(document).ready(function(){
-    $("#form").submit(function() { //устанавливаем событие отправки для формы с id=form
-            var form_data = $(this).serialize(); //собераем все данные из формы
-            $.ajax({
-            type: "POST", //Метод отправки
-            url: "send.php", //путь до php фаила отправителя
-            data: form_data,
-            success: function() {
-                   //код в этом блоке выполняется при успешной отправке сообщения
-                   alert("Ваше сообщение отправлено!");
-            });
-    });
-}); 
+$('[class="subscribe"]').click(function(){
+	$.post(
+		"send.php",
+		{
+			email: $('[name="email"]').val(),
+		},
+		function( data ){
+			$( ".result" ).html(data);
+		}
+	);
+});
